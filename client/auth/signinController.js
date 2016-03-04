8f110972db0d475bebe8141d15c01e52c0287b75
Michelle <mtlassig@gmail.com>
2016-03-04 13:13:13 -0600

    (FIX) Passes user to signinController on pass or fail

--- client/auth/signinController.js
+++ client/auth/signinController.js
@@ -2,11 +2,20 @@
 // invokes function from mainFactory.js to sign in a user
 myApp.controller('SigninController', function ($scope, Auth, $window) {
   $scope.user = {};
+  $scope.failed = null;
   $scope.signin = function (user) {
     Auth.signin(user).then(function (user){
-        Auth.setLoggedIn(true);
+      console.log("User: ", user);
+        if (!user) {
+          $scope.failed = true;
+          return;
+        } else {
+          $scope.failed = false;
+          Auth.setLoggedIn(true);
         // redirect
-        $window.location.href = '/#/create';
+          $window.location.href = '/#/create';
+        }
+        
     });
   };
 });
