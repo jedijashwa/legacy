433046760966b4ba81ac2bee95d8cee225e714ad
Michelle <mtlassig@gmail.com>
2016-03-04 13:23:11 -0600

    (FIX) Upon failed login, clears form. On successful login, redirects to homepage

--- client/auth/signinController.js
+++ client/auth/signinController.js
@@ -2,19 +2,15 @@
 // invokes function from mainFactory.js to sign in a user
 myApp.controller('SigninController', function ($scope, Auth, $window) {
   $scope.user = {};
-  $scope.failed = null;
   $scope.signin = function (user) {
     Auth.signin(user).then(function (user){
-      console.log("User: ", user);
         if (!user) {
-          $scope.failed = true;
+          $scope.user = {};
           return;
-        } else {
-          $scope.failed = false;
-          Auth.setLoggedIn(true);
-        // redirect
-          $window.location.href = '/#/create';
-        }
+        } 
+        Auth.setLoggedIn(true);
+      // redirect
+        $window.location.href = '/#/';
         
     });
   };
