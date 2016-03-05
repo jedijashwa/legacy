9b332e5928c61a82a6ee4adae55a0b6d2dc909d1
Michelle <mtlassig@gmail.com>
2016-03-05 13:08:00 -0600

    (FEAT) sets correct session variables

--- client/profile/profileController.js
+++ client/profile/profileController.js
@@ -1,4 +1,4 @@
-myApp.controller(['$scope', 'Auth', 'Profile', function($scope, Auth, Profile) {
+myApp.controller('ProfileController', ['$scope', 'Auth', 'Profile', function($scope, Auth, Profile) {
   //need to add script to index.html
   //update routes in app.js to include profile page
   $scope.userId = Auth.getUserId();
@@ -7,13 +7,13 @@ myApp.controller(['$scope', 'Auth', 'Profile', function($scope, Auth, Profile) {
   $scope.getStudentSessions = function(){
     Profile.getStudentSessions($scope.userId)
     .then(function(studentSessions) {
-      $scope.studentSessions = studentSessions  
+      $scope.studentSessions = studentSessions.studentSessions;  
     });
   };
   $scope.getTutorSessions = function(){
     Profile.getTutorSessions($scope.userId)
     .then(function(tutorSessions) {
-      $scope.tutorSessions = tutorSessions  
+      $scope.tutorSessions = tutorSessions.tutorSessions;
     });
   };
   $scope.getStudentSessions();
